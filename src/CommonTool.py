2009f47b31a1bca79b2e7537da4fa8879fe39927
fl0w <flowjacky@gmail.com>
2018-07-27 07:28:27 +0800

    change float decimals

--- src/CommonTool.py
+++ src/CommonTool.py
@@ -18,7 +18,7 @@ class CommonTool:
         assert type('str')==type(sciStr),'invalid format'
         if 'E' not in sciStr:
             return sciStr
-        s = '%.8f' % float(sciStr)
+        s = '%.10f' % float(sciStr)
         while '0' == s[-1] and '.' in s:
             s = s[:-1]
         if '.' == s[-1]:
--- www/tools.py
+++ www/tools.py
@@ -12,7 +12,7 @@ from random import randint
 def sci_to_str(sciStr):
     '''科学计数法转换成字符串'''
     assert type('str')==type(sciStr),'invalid format'
-    s = '%.8f' % float(sciStr)
+    s = '%.10f' % float(sciStr)
     while '0' == s[-1] and '.' in s:
         s = s[:-1]
     if '.' == s[-1]:
