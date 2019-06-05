928b24090fd565161c7d3c95b0d5ed0b53343905
fl0wjacky <flowjacky@gmail.com>
2019-06-05 18:28:05 +0800

    NEO Tool add sign

--- www/NEO/tools.py
+++ www/NEO/tools.py
@@ -362,6 +362,24 @@ class Tool:
     def get_transaction_ontology(cpubkey, signature, transaction):
         return transaction + '014140' + signature + '2321' + cpubkey + 'ac'
 
+    @staticmethod
+    def sign(message, private_key):
+        """refer:https://github.com/CityOfZion/neo-python-core/blob/master/neocore/Cryptography/Crypto.py"""
+        bitcoin.change_curve(
+            int("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF", 16),
+            int("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", 16),
+            int("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC", 16),
+            int("5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", 16),
+            int("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", 16),
+            int("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", 16)
+        )
+        hash = hashlib.sha256(binascii.unhexlify(message)).hexdigest()
+        v, r, s = bitcoin.ecdsa_raw_sign(hash, private_key)
+        rb = bytearray(r.to_bytes(32, 'big'))
+        sb = bytearray(s.to_bytes(32, 'big'))
+        sig = rb + sb
+        return sig.hex()
+
     @classmethod
     def verify(cls, cpubkey, signature, message):
         '''refer:https://github.com/cityofzion/neo-python/neo/Cryptography/Crypto.py'''
